479e7639f3c67a0f26fcacb9bf3d2a00b46eb5a8
Einar Ingebrigtsen <einar@dolittle.com>
2012-04-20 20:46:35 +0200

    Trying to put MD file in a content folder - will this work with AppHarbor??

--- Source/Bifrost.Pages.Web/index.js
+++ Source/Bifrost.Pages.Web/index.js
@@ -23,7 +23,7 @@
 		  */
 		});		
 		
-		$.get("Specifications.md", function(e) {
+		$.get("content/Specifications.md", function(e) {
 			var markUp = $("<div/>").append($(marked(e)));
 			
 			$("code",markUp).addClass("prettyprint");

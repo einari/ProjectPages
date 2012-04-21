9ce5a8eaf761b2875c0c73aa5d8a192eada64f21
Einar Ingebrigtsen <einar@dolittle.com>
2012-04-21 07:41:20 +0200

    Deeplink support. :)

--- Source/Bifrost.Pages.Web/index.js
+++ Source/Bifrost.Pages.Web/index.js
@@ -5,13 +5,28 @@
 		this.feature = ko.observable("home");
 		
 		this.uriChanged(function(uri) {
-			self.feature(uri.path.substr(1));
-			prettyPrint();
+			self.setFeatureFromUrl(uri);
 		});
 		
 		
+		this.setFeatureFromUrl = function(uri) {
+			var featureName = uri.path.substr(1);
+			if( featureName && featureName.length > 0 ) {
+				self.feature(featureName);
+			}
+			
+			//prettyPrint();
+		}
+		
+		
+		$(function() {
+			var state = History.getState();
+			self.uri.setLocation(state.url);
+			self.setFeatureFromUrl(self.uri);
+		});
 	}
 	
 	Bifrost.features.ViewModel.baseFor(index);
 	ko.applyBindings(new index());
+	
 })();

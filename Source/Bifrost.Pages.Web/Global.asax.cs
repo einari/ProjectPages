6f4fd031a1b251bc9dd744fa36fd22a5a53dade7
Einar Ingebrigtsen <einar@dolittle.com>
2012-04-22 20:59:39 +0200

    Started working on a busy indicator

--- Source/Bifrost.Pages.Web/Global.asax.cs
+++ Source/Bifrost.Pages.Web/Global.asax.cs
@@ -3,14 +3,30 @@ using System.Collections;
 using System.ComponentModel;
 using System.Web;
 using System.Web.SessionState;
+using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+//[assembly: WebActivator.PreApplicationStartMethod(typeof(BifrostPages.Global), "Start")]
+[assembly: WebActivator.PostApplicationStartMethod(typeof(BifrostPages.Global), "Start")]
 
 namespace BifrostPages
 {
 	public class Global : System.Web.HttpApplication
 	{
+		public static void Start()
+		{
+				var instance = HttpContext.Current.ApplicationInstance;
+			var i=0;
+			i++;
+			//DynamicModuleUtility.RegisterModule()
+		}
+		
+		
 		
 		protected virtual void Application_Start (Object sender, EventArgs e)
 		{
+			var i=0;
+			i++;
+			
 		}
 		
 		protected virtual void Session_Start (Object sender, EventArgs e)

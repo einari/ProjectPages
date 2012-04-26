76f7704254368714f8332ef15bdd4400680981d7
Einar Ingebrigtsen <einar@dolittle.com>
2012-04-26 11:45:05 +0200

    Changing how Disqus is hooked up for identifier - crossing my fingers for it working

--- Source/Bifrost.Pages.Web/features/documentation/viewModel.js
+++ Source/Bifrost.Pages.Web/features/documentation/viewModel.js
@@ -1,49 +1,3 @@
-ko.bindingHandlers.disqus = {
-    init: function (element, valueAccessor, allBindingAccessor, viewModel) {
-    },
-    update: function (element, valueAccessor, allBindingAccessor, viewModel) {
-    	var configuration = valueAccessor();
-    	var identifier = "";
-    	try {
-    		identifier = configuration.identifier;
-    	} catch(e) {
-    		return;
-    	}
-    	
-    	if( identifier.indexOf("undefined") != -1 ) {
-    		return;
-    	}
-    	
-		var script = $("<script type='text/javascript'/>")
-			.text(
-				"var disqus_shortname = 'bifrost';" +
-				"var disqus_identifier = '"+identifier+"';"+
-				"(function() {"+
-				"	var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;"+
-				"	dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';"+
-				"	(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);"+
-				"})();"
-				);
-				
-		$(element).append(script);
-
-
-    	    	/*    	    	
-				<script type="text/javascript">
-				    var disqus_shortname = 'bifrost'; 
-				
-				    (function() {
-				        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-				        dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
-				        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-				    })();
-				</script>		
-    	*/
-    	
-    }
-};
-
-
 (function (undefined) {
 	Bifrost.features.featureManager.get("documentation").defineViewModel(function () {
 		var self = this;
@@ -64,14 +18,27 @@ ko.bindingHandlers.disqus = {
 		
 		
 		this.currentElement = ko.observable({
-			name: ko.observable()
+			name: ko.observable("")
 		});
 		this.currentGroup = ko.observable({
-			name: ko.observable()
+			name: ko.observable("")
 		});
 		this.currentTopic = ko.observable({
-			name: ko.observable(),
+			name: ko.observable(""),
 			elements: ko.observableArray()
+		});
+		this.commentIdentifier = ko.computed(function() {
+			var identifier = self.currentGroup().name() + "_" +
+				self.currentTopic().name() + "_" +
+				self.currentElement().name();
+			return identifier;
+		});
+		
+		this.hasSelectedContent = ko.computed(function() {
+			var enabled = self.currentGroup().name().length > 0 &&
+				self.currentTopic().name().length > 0 &&
+				self.currentElement().name().length > 0;
+			return enabled;
 		});
 		
 		this.content = ko.observable("");
@@ -96,8 +63,15 @@ ko.bindingHandlers.disqus = {
 	
 				});
 				
-				
 				self.content($(markUp).html());
+				
+				DISQUS.reset({
+				  reload: true,
+				  config: function () {  
+				    this.page.identifier = self.commentIdentifier;  
+				    //this.page.url = "http://example.com/#!newthread";
+				  }
+				});				
 				
 				prettyPrint();
 			}, "text");

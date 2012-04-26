23fc3e34a774196cf52cc1f44dffa5acc1cbb9a1
Einar Ingebrigtsen <einar@dolittle.com>
2012-04-26 11:24:09 +0200

    Adding Disqus to documentation!

--- Source/Bifrost.Pages.Web/features/documentation/viewModel.js
+++ Source/Bifrost.Pages.Web/features/documentation/viewModel.js
@@ -1,3 +1,49 @@
+ko.bindingHandlers.disqus = {
+    init: function (element, valueAccessor, allBindingAccessor, viewModel) {
+    },
+    update: function (element, valueAccessor, allBindingAccessor, viewModel) {
+    	var configuration = valueAccessor();
+    	var identifier = "";
+    	try {
+    		identifier = configuration.identifier;
+    	} catch(e) {
+    		return;
+    	}
+    	
+    	if( identifier.indexOf("undefined") != -1 ) {
+    		return;
+    	}
+    	
+		var script = $("<script type='text/javascript'/>")
+			.text(
+				"var disqus_shortname = 'bifrost';" +
+				"var disqus_identifier = '"+identifier+"';"+
+				"(function() {"+
+				"	var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;"+
+				"	dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';"+
+				"	(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);"+
+				"})();"
+				);
+				
+		$(element).append(script);
+
+
+    	    	/*    	    	
+				<script type="text/javascript">
+				    var disqus_shortname = 'bifrost'; 
+				
+				    (function() {
+				        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+				        dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+				        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+				    })();
+				</script>		
+    	*/
+    	
+    }
+};
+
+
 (function (undefined) {
 	Bifrost.features.featureManager.get("documentation").defineViewModel(function () {
 		var self = this;
@@ -24,6 +70,7 @@
 			name: ko.observable()
 		});
 		this.currentTopic = ko.observable({
+			name: ko.observable(),
 			elements: ko.observableArray()
 		});
 		
@@ -62,7 +109,9 @@
 			
 			self.content("");
 			
-			self.loadSample(topic.elements()[0].file());
+			if( topic.elements().length > 0 ) {
+				self.selectElement(topic.elements()[0]);
+			}
 		}
 		
 		this.selectElement = function(element) {

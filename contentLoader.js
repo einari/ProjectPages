0a12487250f6504f530667ca8a6d2ca6485cb7d2
Einar Ingebrigtsen <einar@dolittle.com>
2012-05-01 08:27:01 +0200

    Made it a function..

--- contentLoader.js
+++ contentLoader.js
@@ -1,4 +1,7 @@
-require("text!https://raw.github.com/dolittlestudios/Bifrost-Documentation/master/Web/Single%20Page%20Applications/Decoupling.md", function(v) {
-	theStuff = v;
-	console.log("We're there");
-});
+function loadStuff()
+{
+	require("text!https://raw.github.com/dolittlestudios/Bifrost-Documentation/master/Web/Single%20Page%20Applications/Decoupling.md", function(v) {
+		theStuff = v;
+		console.log("We're there");
+	});
+}

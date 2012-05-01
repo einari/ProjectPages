fb9028a24808378b72eec0ef506b2e65d7e4df89
Einar Ingebrigtsen <einar@dolittle.com>
2012-05-01 08:30:35 +0200

    Testing

--- contentLoader.js
+++ contentLoader.js
@@ -1,6 +1,6 @@
 function loadStuff()
 {
-	require("text!https://raw.github.com/dolittlestudios/Bifrost-Documentation/master/Web/Single%20Page%20Applications/Decoupling.md", function(v) {
+	require("text!index.md", function(v) {
 		theStuff = v;
 		console.log("We're there");
 	});

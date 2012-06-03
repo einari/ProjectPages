9633c92f26f304e53d3847081ffc49115d5d4c87
Einar Ingebrigtsen <einari@Einar-Ingebrigtsens-iMac.local>
2012-06-03 09:43:56 +0200

    Trying out Bifrost locally instead of from the CDN - something is not right with the CDN, its not distributing

--- Source/Bifrost.Pages.Web/scripts/main.js
+++ Source/Bifrost.Pages.Web/scripts/main.js
@@ -9,7 +9,7 @@ require.config({
 	    "jquery": "http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery",
 	    "knockout": "http://cdn.dolittle.com/Knockout/knockout-2.0.0",
 		"knockout.mapping": "http://cdn.dolittle.com/Knockout/knockout.mapping-2.0.0",
-	    "bifrost": "http://cdn.dolittle.com/Bifrost/Bifrost.debug",
+	    "bifrost": "Bifrost.debug", // "http://cdn.dolittle.com/Bifrost/Bifrost.debug",
 	    "order": "http://cdn.dolittle.com/Require/order",
 	    "domReady": "http://cdn.dolittle.com/Require/domReady",
 	    "text": "http://cdn.dolittle.com/Require/text",
